ca7767226a0980807e6a374d8d95a7962e0524fe
Michael Fuget <michaelfuget@gmail.com>
2021-01-24 22:47:26 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -15,6 +15,11 @@ class Song < ActiveRecord::Base
     self.genre = Genre.find_or_create_by(name: name)
   end
 
+  def genre_id=(id)
+    self.genre = Genre.find(id)
+  end
+
+
   def genre_name
     self.genre ? self.genre.name : nil
   end
